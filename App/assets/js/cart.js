96d1c528d9c47b944c805d459c812e1c1fc9cc06
Anna Kyles <annakyles@gmail.com>
2020-11-11 17:52:45 +1300

    Mini cart section styles and function.

--- App/assets/js/cart.js
+++ App/assets/js/cart.js
@@ -1,9 +1,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     
-    // Select attribute
+    // Toggle cart view
+    $('#cart-trigger').on('click mouseover', function() {
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('#cart-content').removeClass('active');
+        } else {
+            $(this).addClass('active');
+            $('#cart-content').addClass('active');
+        }
+    });
+
+    // Select product item attribute
     $('.product-item__attribute-items__attribute-item').on('click', function() {
-        $(this).siblings().removeClass('selected');
-        $(this).addClass('selected');
+        if($(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        } else {
+            $(this).siblings().removeClass('selected');
+            $(this).addClass('selected');
+        }
     });
 
     // Add to Cart
