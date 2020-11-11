9bafea73daabe2b3e3a549cefbf863684e5bdedc
Anna Kyles <annakyles@gmail.com>
2020-11-11 16:55:22 +1300

    Add to cart section attrubute selection function.

--- App/assets/js/cart.js
+++ App/assets/js/cart.js
@@ -1,3 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+    
+    // Select attribute
+    $('.product-item__attribute-items__attribute-item').on('click', function() {
+        $(this).siblings().removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    // Add to Cart
+    $('#add-to-cart').on('click', function() {
+        // if has size attrubute selection, add to cart
+        // else display size selection reminder
+    });
 });
